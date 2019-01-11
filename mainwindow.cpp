089a5549cdf60f04c884a389ffa541159d45bdd8
Jin Huang <jinhuang696@gmail.com>
2019-01-11 15:49:16 -0800

    added captions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this); 
+
     QDir("../Notepad").mkdir("sys");
     QDir("../Notepad").mkdir("saved");
     this->setCentralWidget(ui->centralWidget);
